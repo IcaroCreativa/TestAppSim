985eb01ce23cd0d683436492fa36bb0f5d37eb11
IcaroCreativa <icarocreativa@gmail.com>
2023-01-24 15:06:37 +0100

    install firebase

---
+++ TestAppSim/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5pUxZGiPmJg7eh3AA3wWDqjpVvqW1NPo",
+  authDomain: "friendlyeats-ea74e.firebaseapp.com",
+  projectId: "friendlyeats-ea74e",
+  storageBucket: "friendlyeats-ea74e.appspot.com",
+  messagingSenderId: "1071587310248",
+  appId: "1:1071587310248:web:5e713cd4e1873d6bf6a09d",
+  measurementId: "G-VVLF48VMTW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
